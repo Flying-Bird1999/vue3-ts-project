ac4a22e381d3334ef1c7c71063a93f03b3bb5842
Flying-Bird1999 <1424766809@qq.com>
2021-10-20 14:55:18 +0800

    新建项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <router-link to='/login'>登陆</router-link>
+    <router-link to='/main'>首页</router-link>
+    <router-view></router-view>
+    <el-button>hahaha</el-button>
+    <el-button type="primary">hahaha</el-button>
+    <el-button type="mini">hahaha</el-button>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from "vue"
+
+    export default defineComponent({
+        name: "App",
+    })
+</script>
+
+<style lang="less">
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue"
+import { globalRegister } from "./global"
+
+import App from "./App.vue"
+import router from './router'
+import store from './store'
+
+const app = createApp(App)
+
+// globalRegister()
+app.use(globalRegister)
+app.use(router)
+app.use(store)
+app.mount("#app")
