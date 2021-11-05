7ece2f3c64cfba1a03abdf9d02f05097e7690363
Flying-Bird1999 <1424766809@qq.com>
2021-11-05 13:23:39 +0800

    展示组件的封装

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-    <router-view></router-view>
+    <div class="app">
+        <el-config-provider :locale="zhCn">
+            <router-view></router-view>
+        </el-config-provider>
+    </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue"
+import { defineComponent } from 'vue'
 
-    export default defineComponent({
-        name: "App",
-    })
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        ElConfigProvider
+    },
+    props: {
+        name: {
+            type: String
+        }
+    },
+    setup() {
+        return {
+            zhCn
+        }
+    }
+})
 </script>
 
 <style lang="less">
-    .app {
-        height: 100%;
-    }
+.app {
+  height: 100%;
+}
 </style>
--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -5,7 +5,9 @@ dayjs.extend(utc)
 
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss'
 
+// utc时间转化
 export function formatUtcString(utcString: string, format: string = DATE_TIME_FORMAT) {
     return dayjs.utc(utcString).format(format)
 }
+// 时间戳转化
 
