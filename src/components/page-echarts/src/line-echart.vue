a78f8e21c7b079c334af3d82a646f8c3ca517fdf
Flying-Bird1999 <1424766809@qq.com>
2021-11-11 20:15:24 +0800

    玫瑰、折线、柱形图

---
+++ src/components/page-echarts/src/line-echart.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="line-echart">
+        <base-echart :options="options"></base-echart>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps, withDefaults } from 'vue'
+import BaseEchart from '@/base-ui/echart'
+
+const props = withDefaults(
+    defineProps<{
+        title: string
+        xLabels: string[]
+        values: any[]
+    }>(), {
+        title: '折线图标题'
+    }
+)
+
+const options = computed(() => {
+    return {
+        title: {
+            text: props.title
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: props.xLabels
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                name: '分类销量',
+                data: props.values,
+                type: 'line',
+                areaStyle: {}
+            }
+        ]
+    };
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -10,17 +10,22 @@
                 <xc-card title="不同城市商品销量"></xc-card>
             </el-col>
             <el-col :span="7">
-                <xc-card title="分类商品数量(玫瑰图)"></xc-card>
+                <xc-card title="分类商品数量(玫瑰图)">
+                    <rose-echart :roseData="categoryGoodsCount"></rose-echart>
+                </xc-card>
             </el-col>
         </el-row>
 
         <el-row :gutter="10" class="content-row">
             <el-col :span="12">
                 <xc-card title="分类商品的销量">
+                    <line-echart v-bind="categoryGoodsSale"></line-echart>
                 </xc-card>
             </el-col>
             <el-col :span="12">
-                <xc-card title="分类商品的收藏"></xc-card>
+                <xc-card title="分类商品的收藏">
+                    <bar-echart v-bind="categoryGoodsFavor"></bar-echart>
+                </xc-card>
             </el-col>
         </el-row>
     </div>
@@ -31,26 +36,55 @@ import { computed, defineComponent } from 'vue'
 import { useStore } from 'vuex'
 
 import XcCard from '@/base-ui/card'
-import { PieEchart } from '@/components/page-echarts'
+import { PieEchart, RoseEchart, LineEchart, BarEchart } from '@/components/page-echarts'
 
 export default defineComponent({
     name: 'dashboard',
     components: {
         XcCard,
-        PieEchart
+        PieEchart,
+        RoseEchart,
+        LineEchart,
+        BarEchart
     },
     setup() {
         const store = useStore()
+        // 请求数据
         store.dispatch('dashboard/getDashboardDataAction')
 
+        // 获取数据
         const categoryGoodsCount = computed(() => {
             return store.state.dashboard.categoryGoodsCount.map((item: any) => {
                 return {name: item.name, value: item.goodsCount}
             })
         })
+        
+        const categoryGoodsSale = computed(() => {
+            const xLabels: string[] = []
+            const values: any[] = []
+            const categoryGoodsSale = store.state.dashboard.categoryGoodsSale
+            for (const item of categoryGoodsSale) {
+                xLabels.push(item.name)
+                values.push(item.goodsCount)
+            }
+            return { xLabels, values }
+        })
+
+        const categoryGoodsFavor = computed(() => {
+            const xLabels: string[] = []
+            const values: any[] = []
+            const categoryGoodsFavor = store.state.dashboard.categoryGoodsFavor
+            for (const item of categoryGoodsFavor) {
+                xLabels.push(item.name)
+                values.push(item.goodsFavor)
+            }
+            return { xLabels, values }
+        })
 
         return {
-            categoryGoodsCount
+            categoryGoodsCount,
+            categoryGoodsSale,
+            categoryGoodsFavor
         }
     }
 })
